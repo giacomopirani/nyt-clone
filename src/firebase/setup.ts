bcecb4e7470a9274f1cff82d500c3b8dd683a7cc
Giacomo Pirani <giacomopirani1996@gmail.com>
2024-12-13 15:44:53 +0100

    fixed apiKey firebase

--- src/firebase/setup.ts
+++ src/firebase/setup.ts
@@ -14,11 +14,11 @@ console.log(
 
 const firebaseConfig = {
   apiKey: "process.env.REACT_APP_FIREBASE_API_KEY",
-  authDomain: "nyt-clone-c7195.firebaseapp.com",
-  projectId: "nyt-clone-c7195",
-  storageBucket: "nyt-clone-c7195.appspot.com",
-  messagingSenderId: "450808452291",
-  appId: "1:450808452291:web:6806edf31152d266c1be76",
+  authDomain: "process-env.REACT_APP_FIREBASE_AUTH_DOMAIN",
+  projectId: "process.env.REACT_APP_FIREBASE_PROJECT_ID",
+  storageBucket: "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "process.env.REACT_APP_FIREBASE_APP_ID",
 };
 
 console.log("firebaseConfig:", firebaseConfig);
